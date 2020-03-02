e9deb503c4277f7f763e9e92038c16800ea061ba
Javiert <javierthsm@gmail.com>
2020-03-02 14:42:12 -0300

    Update and rename index.html to popupper.js

---
+++ popupper.js
@@ -0,0 +1,8 @@
+
+function popupper(url, w=360, h=280, x = 0, y = 0){
+    pop=window.open(url,'PopupperJT','height='+h+',width='+w+',top='+y+', left='+x);
+    if (window.focus) {
+        pop.focus()
+    }
+    return false;
+}
