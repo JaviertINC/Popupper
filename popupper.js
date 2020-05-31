9034197eb7bd9272b94aa66ee55dee1326b5a670
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-05-30 20:30:56 -0400

    Update popupper.js

--- popupper.js
+++ popupper.js
@@ -1,5 +1,10 @@
 // Modo de Uso
-
+//
+// <script>
+//      //función("STRING enlace","INT ancho","INT alto","INT posición X", "INT posición Y");
+//      //Ejemplo:
+//      pupupper("https://www.youtube.com/embed/hGYkuhHPc-g", 480, 360, 50, 300);
+// </script>
 
 function popupper(url, w=360, h=280, x = 0, y = 0){
     pop=window.open(url,'PopupperJT','height='+h+',width='+w+',top='+y+', left='+x);
